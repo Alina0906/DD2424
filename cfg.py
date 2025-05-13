3bc0877ce052508998f0256c0be77afe73efddee
Alina0906 <18718418998@163.com>
2025-05-14 00:22:13 +0800

    Use Large Models

--- cfg.py
+++ cfg.py
@@ -9,7 +9,7 @@ class Config:
     num_workers: int = 4
     img_size: int = 224
 
-    model_name: str = "google/vit-base-patch16-224-in21k"
+    model_name: str = "google/vit-large-patch32-384"
     num_labels: int = 37
     task: str = "class"
 
